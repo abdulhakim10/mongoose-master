dec941340ddb445df8e9ca12623bdf267c0803ad
syed abdul hakim <s.abdulhakim10@gmail.com>
2023-05-18 21:37:04 +0600

    create and test post route, refactor code

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,23 +1,13 @@
+import { IUser } from "./user.interface";
 import User from "./user.model";
 
-export const createUserToDB = async () => {
-    const user = await new User({
-        id: "234",
-        role: "student",
-        password: "58498",
-        name: {
-            firstName: "Md",
-            middleName: "Janam",
-            lastName: "Khan",
-        },
-        dateOfBirth: "4/9/1988",
-        gender: "male",
-        email: "janam@hamid.com",
-        contactNo: "015438905",
-        emergencyContact: "0184389709",
-        presentAddress: "23/A ghosh road",
-        permanentAddress: "33/C das road uganda",
-      });
+export const createUserToDB = async (payload: IUser): Promise<IUser> => {
+    const user = await new User(payload);
       await user.save();
       return user;
 };
+
+export const getUserFromDB = async (): Promise<IUser[]> => {
+    const users = await User.find({});
+    return users;
+}
